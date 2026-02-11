b46279d7431dff14c26118d79055dba28b82d740
FIreLion-08 <Works-dim@yandex.ru>
2026-02-11 19:10:47 +0800

    HW-02_v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import './globals.css';
-import ReduxProvider from '../store/ReduxProvider';
 
 const montserrat = Montserrat({
     variable: '--font-montserrat',
@@ -19,10 +18,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ReduxProvider>
-            <html lang="ru">
-                <body className={`${montserrat.variable}`}>{children}</body>
-            </html>
-        </ReduxProvider>
+    <html lang="ru">
+      <body className={`${montserrat.variable}`}>{children}</body>
+    </html>
     );
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,45 +1,35 @@
+'use client';
+
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './nav.module.css';
+import NavMenu from './nav-menu/NavMenu';
+import { useState } from 'react';
 
 export default function Nav() {
-  return (
-    <nav className={styles.main__nav}>
-      <div className={styles.nav__logo}>
-        {/*TODO: img –> Image*/}
-        <Image
-          width={250}
-          height={170}
-          className={styles.logo__image}
-          src="/img/logo.png"
-          alt={'logo'}
-        />
-      </div>
-      <div className={styles.nav__burger}>
-        <span className={styles.burger__line}></span>
-        <span className={styles.burger__line}></span>
-        <span className={styles.burger__line}></span>
-      </div>
-      <div className={styles.nav__menu}>
-        <ul className={styles.menu__list}>
-          <li className={styles.menu__item}>
-            {/*TODO: a -> Link*/}
-            <Link href="#" className={styles.menu__link}>
-              Главное
-            </Link>
-          </li>
-          <li className={styles.menu__item}>
-            <Link href="#" className={styles.menu__link}>
-              Мой плейлист
-            </Link>
-          </li>
-          <li className={styles.menu__item}>
-            <Link href="../sign-in" className={styles.menu__link}>
-              Войти
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+    const [openNavMenu, setOpenNavMenu] = useState(false);
+    return (
+        <nav className={styles.main__nav}>
+            <div className={styles.nav__logo}>
+                {/*TODO: img –> Image*/}
+                <Image
+                    width={250}
+                    height={170}
+                    className={styles.logo__image}
+                    src="/img/logo.png"
+                    alt={'logo'}
+                />
+            </div>
+            <div
+                className={styles.nav__burger}
+                onClick={() => {
+                    setOpenNavMenu(!openNavMenu);
+                }}
+            >
+                <span className={styles.burger__line}></span>
+                <span className={styles.burger__line}></span>
+                <span className={styles.burger__line}></span>
+            </div>
+            {openNavMenu && <NavMenu />}
+        </nav>
+    );
 }
